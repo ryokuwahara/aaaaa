cf206ca884067467c7687b5e528dca8748a41601
DShintaro <sauli0419@gmail.com>
2013-12-23 19:09:38 +0900

    コメントの追加

--- src/com/example/aaaaa/MainActivity.java
+++ src/com/example/aaaaa/MainActivity.java
@@ -3,6 +3,15 @@ package com.example.aaaaa;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+
+
+// 今回の課題は
+// ペアになった人が作ったプロジェクトをcloneする
+// そのプロジェクトにコメントを残す
+// コメントを残した変更に対して、コミットを行う
+// 変更をリモートサーバーへプッシュして伝える。
+// ペアの人にその変更を受け取ってもらう※プル(pull)する。
+
 //git rensyu
 //git rensyu2
 public class MainActivity extends Activity {
@@ -12,6 +21,5 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-
 
 }
